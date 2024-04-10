fb7e269404ef3be6ab0eebef62a4773c72189bfc
Joáo Oliveira <1191457@isep.ipp.pt>
2024-04-10 16:37:12 +0100

    Phishing Minigame implementation start

--- Assets/Scripts/MultiplayerMenuScripts/GameShowSceneManager.cs
+++ Assets/Scripts/MultiplayerMenuScripts/GameShowSceneManager.cs
@@ -5,9 +5,9 @@ using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 
-public class MP_GameManager : NetworkBehaviour
+public class GameShowSceneManager : NetworkBehaviour
 {
-    public static MP_GameManager Instance { get; private set; }
+    public static GameShowSceneManager Instance { get; private set; }
 
     [Header("VISUAL STUFF")]
     [SerializeField] private GameObject loadingUI;
