4c136ac1a4eda7752d4d2d5ac437c99f59f119d1
Hasuru <hugo.as.silva25@gmail.com>
2024-04-26 13:36:01 +0100

    (UPDATED HelperPassword Scene) Added additional navigation options to the protocols

--- Assets/Scripts/HelperPassword/HelperController.cs
+++ Assets/Scripts/HelperPassword/HelperController.cs
@@ -10,11 +10,22 @@ public class HelperController : MonoBehaviour
 
     private int protocolIndex = 0;
 
-    public void UpdateProtocol()
+    void Update()
     {
-        protocolIndex++;
+        if (Input.GetKeyDown(KeyCode.D))
+            UpdateProtocol(1);
+        else if (Input.GetKeyDown(KeyCode.A))
+            UpdateProtocol(-1);
+    }
+
+    public void UpdateProtocol(int direction)
+    {
+        protocolIndex += direction;
+
         if (protocolIndex >= protocols.Length)
             protocolIndex = 0;
+        if (protocolIndex < 0)
+            protocolIndex = protocols.Length - 1;
         
         currentProtocol.sprite = protocols[protocolIndex];
     }
