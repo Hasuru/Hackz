8fbe12bab723b8b75a0c003c0a85d707459a2eed
Hasuru <hugo.as.silva25@gmail.com>
2024-04-30 11:36:37 +0100

    (DELETED & UPDATED Question Phase) Start of refactorization of the Question Phase scene | Added some initial assets to Question Phase | added initial Question UI initialization

--- Assets/Scripts/QuestionPhase/Q_Utility.cs
+++ Assets/Scripts/QuestionPhase/Q_Utility.cs
@@ -1,9 +1,2 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Q_Utility
-{
-    public const float ResolutionDelayTime = 1.0f;
-    public const string SavePrefKey = "Game_HighScore_Value";
-}
+public enum CategoryType { PHISHING, PASSWORD }
+public enum GameState { CHOOSING, LOCKED, SWITCH, FINISHED }
