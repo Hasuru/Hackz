d25b2766954e910f55d4f8c1d45a54affeb83c8f
Hasuru <hugo.as.silva25@gmail.com>
2024-05-03 13:32:16 +0100

    (Updated Overall) added preferences script and additional assets

---
+++ Assets/Scripts/GamePreferencesManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePreferencesManager : MonoBehaviour
+{
+    const string scoreKey = "score";
+    const string categoryKey = "category";
+    const string timePUKey = "timePowerUp";
+    const string cutPUKey = "cutPowerUp";
+
+    [SerializeField] Q_GameManager _gameManager;
+
+    public void SavePrefs(int score, string category, int timePU, int cutPU)
+    {
+        PlayerPrefs.SetInt(scoreKey, score);
+        PlayerPrefs.SetString(categoryKey, category);
+        PlayerPrefs.SetInt(timePUKey, timePU);
+        PlayerPrefs.SetInt(cutPUKey, cutPU);
+        PlayerPrefs.Save();
+    }
+
+    public void LoadPrefs()
+    {
+        _gameManager.Points = PlayerPrefs.GetInt(scoreKey, 0);
+        _gameManager.PowerUps[0] = PlayerPrefs.GetInt(timePUKey, 0);
+        _gameManager.PowerUps[1] = PlayerPrefs.GetInt(cutPUKey, 0);
+
+        var category = PlayerPrefs.GetString(categoryKey, "PASSWORD");
+        if (category.Equals("PASSWORD"))
+            _gameManager.Category = CategoryType.PASSWORD;
+        else
+            _gameManager.Category = CategoryType.PHISHING;
+    }
+}
